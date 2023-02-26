9d1b35e5130f14da60f9ac132ed2fff73feb06d7
ngimdock <ngimdockzemfack@gmail.com>
2023-02-26 10:23:25 +0100

    Adding the hashing service

---
+++ src/iam/iam.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { HashingService } from './hashing.service';
+import { BcryptService } from './bcrypt.service';
+
+@Module({
+  providers: [
+    {
+      provide: HashingService,
+      useClass: BcryptService,
+    },
+  ],
+})
+export class IamModule {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+}
